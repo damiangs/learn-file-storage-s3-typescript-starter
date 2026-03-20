6d7e6656cd59664b17d5ad939417510aa45be8ac
damiangs <cgsdamian@gmail.com>
2026-03-19 19:13:06 -0600

    initial commit

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,4 +1,5 @@
 import { existsSync, mkdirSync } from "fs";
+import path from "path";
 
 import type { ApiConfig } from "../config";
 
@@ -6,4 +7,25 @@ export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
+}
+
+export function getAssetURL(cfg: ApiConfig, assetPath: string) {
+  return `http://localhost:${cfg.port}/assets/${assetPath}`;
+}
+
+export function mediaTypeToExt(str: string) {
+  const parts = str.split("/");
+
+  if (parts.length !== 2) {
+    return ".bin";
+  }
+  const extension = "." + parts[1];
+
+  return extension;
+}
+
+export function getAssetDiskPath(cfg: ApiConfig, filename: string) {
+  const fullDiskPath = path.join(cfg.assetsRoot, filename);
+
+  return fullDiskPath;
 }
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { getAssetURL, mediaTypeToExt, getAssetDiskPath } from "./assets";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -48,6 +49,39 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 20;
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Upload file too large");
+  }
+
+  const mediaType = file.type;
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
+  }
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Video not found");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
+
+  const extension = mediaTypeToExt(mediaType);
+  const fileName = videoId + extension;
+  const diskPath = getAssetDiskPath(cfg, fileName);
+
+  await Bun.write(diskPath, file);
+
+  video.thumbnailURL = getAssetURL(cfg, fileName);
+
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
